71097f3634fd898631749205e1021bb2ff8d1afe
Artura11 <artura11@gmail.com>
2022-08-21 20:39:51 +0300

    fhh

--- stop-watch/step-05/stop-watch.js
+++ stop-watch/step-05/stop-watch.js
@@ -1,7 +1,10 @@
 
 const stopWatchView = document.querySelector('#stop-watch-view');
-const toggleStopWatchButton = document
-    .querySelector('#toggle-stop-watch').addEventListener('click', toggleStopWatch);
+const toggleStopWatchButton = document.querySelector('#toggle-stop-watch');
+toggleStopWatchButton.addEventListener('click', toggleStopWatch);
+
+const resetButton = document.getElementById('reset-stop-watch');
+resetButton.addEventListener("click", resetStopWatch);
 
 let hundreth = 0;
 let seconds = 0;
@@ -13,7 +16,13 @@ function displayTime() {
     stopWatchView.innerHTML = `${minutes}:${seconds}:${hundreth}`;
 }
 
+function resetStopWatch() {
+    hundreth = 0;
+    seconds = 0;
+    minutes = 0;
 
+    displayTime();
+}
 function handleTimeChange() {
     hundreth++;
     if (hundreth > 99) {
@@ -28,15 +37,25 @@ function handleTimeChange() {
 
 }
 
+function startStopWatch() {
+    intervalId = setInterval(handleTimeChange, 10);
+    toggleStopWatchButton.innerHTML = 'Stop';
+}
+function stopStopWatch() {
+    clearInterval(intervalId);
+    toggleStopWatchButton.innerHTML = 'Start';
+    intervalId = null;
+}
+
+
 let intervalId = null;
 function toggleStopWatch() {
     if (intervalId == null) {
-        intervalId = setInterval(handleTimeChange, 10);
-        toggleStopWatchButton.innerHTML = 'Stop';
+        startStopWatch();
     } else {
-        clearInterval(intervalId);
-        toggleStopWatchButton.innerHTML = 'Start';
-        intervalId = null;
+        stopStopWatch();
 
     }
 }
+
+
