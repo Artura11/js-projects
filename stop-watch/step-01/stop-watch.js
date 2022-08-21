037713da377720510f42ee6f51454b541acaccdd
Artura11 <artura11@gmail.com>
2022-08-21 18:10:22 +0300

    asd

---
+++ stop-watch/step-01/stop-watch.js
@@ -0,0 +1,17 @@
+
+const stopWatchView = document.querySelector('#stop-watch-view');
+
+console.log('id', stopWatchView);
+
+const pTag = document.querySelector('p');
+console.log('pTag', pTag);
+
+const myClass = document.querySelector('.my-class');
+console.log('class', myClass);
+
+const allParagraph = document.querySelectorAll('p');
+console.log(allParagraph);
+
+console.log('p1', allParagraph[1]);
+
+console.log('p2', allParagraph[2]);
