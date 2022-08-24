d86b7b9cd61453165e347b39b910362eca60d33c
Artura11 <artura11@gmail.com>
2022-08-24 19:50:49 +0300

    12

---
+++ Timers/step-04/app.js
@@ -0,0 +1,12 @@
+import Timer from './timers.js';
+
+/* console.log(Timer); */
+
+document.querySelector('#create-timer').addEventListener('click', addTimer);
+const timerInput = document.querySelector('#time-input');
+
+
+function addTimer() {
+    const initialTime = timerInput.value;
+    console.log('value from input', initialTime);
+}
