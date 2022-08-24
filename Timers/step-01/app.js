31890aae4e228e0a4c8ef9caa40e2d154aee83f4
Artura11 <artura11@gmail.com>
2022-08-24 17:55:12 +0300

    ssss

---
+++ Timers/step-01/app.js
@@ -0,0 +1,4 @@
+import StamName, { name, number } from './timers';
+console.log('name', name);
+console.log('obj', StamName);
+console.log('number', number);
---
+++ Timers/step-01/timers.js
@@ -0,0 +1,10 @@
+export const name = "artur";
+
+export const number = 24;
+
+const obj = {
+    name: 'artur',
+    last_name: 'brener'
+};
+
+export default obj;
